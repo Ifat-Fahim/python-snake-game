43e4a56a57a3524059e777e3256d7ef7034214cf
Ifat-Fahim <ifatfahim007@gmail.com>
2021-10-12 20:30:17 +0600

    keep track of the high score

--- scoreboard.py
+++ scoreboard.py
@@ -3,11 +3,12 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 15, "bold")
 
-
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -16,13 +17,18 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Your Score: {self.score}", False, align=ALIGNMENT,
+        self.clear()
+        self.write(f"Your Score: {self.score} | High Score: {self.high_score}", False, align=ALIGNMENT,
                    font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game over", False, align=ALIGNMENT,
-                   font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
